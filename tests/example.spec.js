5f01d4c5cf845ec5fcb9fa47050ab769ae35ef12
Philip Müller <philipmuller97@gmail.com>
2024-05-13 16:25:44 +0300

    Complete 5.17: Blog List End To End Testing, step 1

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,12 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect, beforeEach, describe } = require('@playwright/test')
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  test('Login form is shown by default', async ({ page }) => {
+    const locator = await page.getByText('Log in')
+    expect(locator).toBeVisible()
+  })
+})
