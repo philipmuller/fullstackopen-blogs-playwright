322d1adf9264e6b9e84a0bcdb7e620117c4d33f4
Philip Müller <philipmuller97@gmail.com>
2024-05-13 23:38:35 +0300

    Solve 5.18: Blog List End To End Testing, step 2

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,8 +1,20 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
+//Note: for end to end tests to work, the server needs to be in test mode connected to the test database (NODE_ENV=test)
+
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  let user = {
+    username: 'play',
+    password: 'play',
+    name: 'Playwright'
+  }
+  let baseURL = 'http://localhost:5173'
+
+  beforeEach(async ({ page, request }) => {
+    await request.post(`${baseURL}/api/testing/reset`)
+    await request.post(`${baseURL}/api/users`, { data: user })
+
+    await page.goto(baseURL)
   })
 
   test('Login form is shown by default', async ({ page }) => {
@@ -10,21 +22,26 @@ describe('Blog app', () => {
     await expect(locator).toBeVisible()
   })
 
-  // describe('Login', () => {
-  //   test('succeeds with correct credentials', async ({ page }) => {
-  //     await page.fill('input[name="username"]', 'root')
-  //     await page.fill('input[name="password"]', 'sekret')
-  //     await page.click('text=login')
-  //     const locator = await page.getByText('root logged in')
-  //     expect(locator).toBeVisible()
-  //   })
+  describe('Login', () => {
+    
+    test('succeeds with correct credentials', async ({ page }) => {
+      const texboxes = await page.getByRole('textbox').all()
+      await texboxes[0].fill(user.username)
+      await texboxes[1].fill(user.password)
 
-  //   test('fails with wrong credentials', async ({ page }) => {
-  //     await page.fill('input[name="username"]', 'root')
-  //     await page.fill('input[name="password"]', 'wrong')
-  //     await page.click('text=login')
-  //     const locator = await page.getByText('wrong username or password')
-  //     expect(locator).toBeVisible()
-  //   })
-  // })
+      await page.click('text=login')
+      const locator = await page.getByText('Playwright logged-in')
+      await expect(locator).toBeVisible()
+    })
+
+    test('fails with incorrect credentials', async ({ page }) => {
+      const texboxes = await page.getByRole('textbox').all()
+      await texboxes[0].fill("random")
+      await texboxes[1].fill("false")
+
+      await page.click('text=login')
+      const locator = await page.getByText('Wrong username or password')
+      await expect(locator).toBeVisible()
+    })
+  })
 })
