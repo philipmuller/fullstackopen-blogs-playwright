9db15373951ffd14ad7cbf68b8232da0d783ffb4
Philip Müller <philipmuller97@gmail.com>
2024-05-13 16:41:05 +0300

    Fix missing await in test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,6 +7,24 @@ describe('Blog app', () => {
 
   test('Login form is shown by default', async ({ page }) => {
     const locator = await page.getByText('Log in')
-    expect(locator).toBeVisible()
+    await expect(locator).toBeVisible()
   })
+
+  // describe('Login', () => {
+  //   test('succeeds with correct credentials', async ({ page }) => {
+  //     await page.fill('input[name="username"]', 'root')
+  //     await page.fill('input[name="password"]', 'sekret')
+  //     await page.click('text=login')
+  //     const locator = await page.getByText('root logged in')
+  //     expect(locator).toBeVisible()
+  //   })
+
+  //   test('fails with wrong credentials', async ({ page }) => {
+  //     await page.fill('input[name="username"]', 'root')
+  //     await page.fill('input[name="password"]', 'wrong')
+  //     await page.click('text=login')
+  //     const locator = await page.getByText('wrong username or password')
+  //     expect(locator).toBeVisible()
+  //   })
+  // })
 })
